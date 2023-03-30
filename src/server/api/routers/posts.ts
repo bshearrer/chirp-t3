4ee1c1713db08cc4762a9cd725a9e371c183dd75
Brandon Shearrer <bshearr@outlook.com>
2023-03-29 22:19:43 -0500

    implement rate limiter

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,13 +1,25 @@
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
 import { z } from "zod";
-
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
     return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl}
 }
+
+
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -45,6 +57,12 @@ export const postsRouter = createTRPCRouter({
     content: z.string().emoji().min(1).max(280),
   })).mutation(async ({ ctx, input }) => {
     const authorId = ctx.userId
+
+    const { success } = await ratelimit.limit(authorId);
+
+    if (!success) {
+        throw new TRPCError({code: "TOO_MANY_REQUESTS", message: "You are posting too fast. Please slow down."})
+    }
 
     const post = await ctx.prisma.post.create({
         data: {
